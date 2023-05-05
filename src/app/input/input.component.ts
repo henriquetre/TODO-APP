faa120a78812e0798133c8c4837bf53cfeeb438c
HENRIQUE GONÇALVES DE JESUS <henrique_g_jesus@estudante.sesisenai.org.br>
2023-05-04 21:52:35 -0300

    Segunda entrega

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,10 +1,11 @@
 import { Component, Output, EventEmitter, OnInit } from "@angular/core";
-import { T } from "@angular/core/src/render3";
-import { forEach } from "@angular/router/src/utils/collection";
+
 
 
 interface Tarefa{
     Descricao: string;
+    Categoria: string;
+    editando:  false;
 }
 
 @Component({
@@ -17,10 +18,20 @@ interface Tarefa{
 })
 export class InputComponent implements OnInit{
   ngOnInit() {
-    const lista: Tarefa[] = JSON.parse(localStorage.getItem('listaTarefas'));
-    if (lista != null) {
-      this.listaTarefas = lista;
-      console.log(lista);
+    const lista1: Tarefa[] = JSON.parse(localStorage.getItem('listaTodo'));
+    const lista2: Tarefa[] = JSON.parse(localStorage.getItem('listaDone'));
+    const lista3: Tarefa[] = JSON.parse(localStorage.getItem('listaDoing'));
+    if (lista1 != null) {
+      this.listaTodo = lista1;
+      console.log(lista1);
+    }
+    if (lista2 != null) {
+      this.listaDone = lista2;
+      console.log(lista2);
+    }
+    if (lista3 != null) {
+      this.listaDoing = lista3;
+      console.log(lista3);
     }
   }
   
@@ -28,10 +39,16 @@ export class InputComponent implements OnInit{
     @Output() click = new EventEmitter<Tarefa>();
 
     tarefa: Tarefa = {
-      Descricao: ""
+      Descricao: "",
+      Categoria: "",
+      editando: false
     };
 
     listaTarefas: Tarefa[]=[];
+
+    listaTodo: Tarefa[]=[];
+    listaDone: Tarefa[]=[];
+    listaDoing: Tarefa[]=[];
 
     tarefaAtual: Tarefa = null;
 
@@ -39,14 +56,30 @@ export class InputComponent implements OnInit{
 
     adicionarTarefa(): void{
         this.click.emit(this.tarefa);
-
+        console.log(this.listaDone)
+        console.log(this.listaTodo)
+        console.log(this.listaDoing)
         const novaTarefa: Tarefa = {
-            Descricao: this.tarefa.Descricao
+            Descricao: this.tarefa.Descricao,
+            Categoria: this.tarefa.Categoria,
+            editando:this.tarefa.editando
           };
-          this.listaTarefas.push(novaTarefa);
-        console.log(this.listaTarefas)
+      
 
-        localStorage.setItem('listaTarefas', JSON.stringify(this.listaTarefas));
+        if(this.tarefa.Categoria==="TODO"){
+          this.listaTodo.push(novaTarefa);
+        }
+        if(this.tarefa.Categoria==="DONE"){
+          this.listaDone.push(novaTarefa);
+        }
+        if(this.tarefa.Categoria==="DOING"){
+          this.listaDoing.push(novaTarefa);
+          console.log(this.listaDoing)
+        }
+        
+        localStorage.setItem('listaTodo', JSON.stringify(this.listaTodo));
+        localStorage.setItem('listaDone', JSON.stringify(this.listaDone));
+        localStorage.setItem('listaDoing', JSON.stringify(this.listaDoing));
         this.tarefa.Descricao="";
     
 
@@ -54,15 +87,37 @@ export class InputComponent implements OnInit{
 
    
     excluirTarefa(tarefa: Tarefa): void {
-      const index = this.listaTarefas.indexOf(tarefa);
+      const index = this.listaTodo.indexOf(tarefa);
+      const index1 = this.listaDoing.indexOf(tarefa);
+      const index2 = this.listaDone.indexOf(tarefa);
       if (index !== -1) {
-        this.listaTarefas.splice(index, 1);
-        
+        this.listaTodo.splice(index, 1);
+      }
+      if(index1!==-1){
+        this.listaDoing.splice(index1,1);
+      }
+      if(index2!==-1){
+        this.listaDone.splice(index2,1);
       }
       
-      localStorage.setItem('listaTarefas', JSON.stringify(this.listaTarefas));
-      localStorage.removeItem(this.tarefa.Descricao);
+      localStorage.setItem('listaTodo', JSON.stringify(this.listaTodo));
+        localStorage.setItem('listaDone', JSON.stringify(this.listaDone));
+        localStorage.setItem('listaDoing', JSON.stringify(this.listaDoing));
+     
     }
-}
+    editarTarefa(tarefaAux) {
+    this.tarefa.Descricao=tarefaAux.Descricao
+    this.tarefa.Categoria=tarefaAux.Categoria
+    this.excluirTarefa(tarefaAux)
+    }
+  
+    salvarTarefa(tarefa) {
+      tarefa.editando = false;
+      localStorage.setItem('listaTodo', JSON.stringify(this.listaTodo));
+        localStorage.setItem('listaDone', JSON.stringify(this.listaDone));
+        localStorage.setItem('listaDoing', JSON.stringify(this.listaDoing));
+    }
+  }
+  
      
 
