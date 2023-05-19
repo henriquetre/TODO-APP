f0e787e6ed1138f3ac754e1cc9509420d99610c3
HENRIQUE GONÇALVES DE JESUS <henrique_g_jesus@estudante.sesisenai.org.br>
2023-05-19 09:51:07 -0300

    19-05

--- src/todo/todo.component.ts
+++ src/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
 
-interface Categoria{
+interface Categoria {
   Nome: string;
   Cor: string;
 }
@@ -13,57 +13,64 @@ interface Categoria{
   styleUrls: ['./todo.component.css']
 })
 
-export class TodoComponent implements OnInit{
-    
-    ngOnInit() {
-const lista1: Categoria[] = JSON.parse(localStorage.getItem('listaCategoria'));
+export class TodoComponent implements OnInit {
+
+  ngOnInit() {
+    const lista1: Categoria[] = JSON.parse(localStorage.getItem('listaCategoria'));
     if (lista1 != null) {
       this.listaCategoria = lista1;
       console.log(lista1);
-    } 
-  
-
-
     }
-    categoria: Categoria = {
-      Nome: "",
-      Cor: "",
-    };
-
-    listaCategoria: Categoria[]=[];
-
-    atualizarLocalStorage():void{
-        console.log('entrei');
-        localStorage.setItem('listaTarefas', JSON.stringify(this.listaCategoria))
-      }
-  
-      adcionarCategoria(): void{
-        const NovaCategoria: Categoria = {
-          Nome: this.categoria.Nome,
-          Cor: this.categoria.Cor
-        };
-        if(NovaCategoria.Nome==""){
-          alert("Insira um nome para a Categoria")
-        }else{
-          console.log(NovaCategoria)
-          this.listaCategoria.push(NovaCategoria)
-          localStorage.setItem('listaCategoria', JSON.stringify(this.listaCategoria));
-          this.categoria.Nome="";
-        }
-       
-       
-       
-  
-      }
 
 
-      excluirCategoria(categoria:Categoria): void {
-        const index = this.listaCategoria.indexOf(categoria);
-  
-        if (index !== -1) {
-          this.listaCategoria.splice(index, 1);
-        }
-        localStorage.setItem('listaCategoria', JSON.stringify(this.listaCategoria))    
-      }
     
+  }
+  
+  categoria: Categoria = {
+    Nome: "",
+    Cor: "",
+  };
+  listaCategoria: Categoria[] = [];
+
+  atualizarLocalStorage(): void {
+    console.log('entrei');
+    localStorage.setItem('listaTarefas', JSON.stringify(this.listaCategoria))
+  }
+
+  adcionarCategoria(): void {
+    if (this.categoria.Cor === "") {
+      this.categoria.Cor = "#000000"
+    }
+    if (this.categoria.Cor === "#FFFFFF") {
+      this.categoria.Cor = "#000000"
+    }
+    console.log(this.categoria.Cor)
+    const NovaCategoria: Categoria = {
+      Nome: this.categoria.Nome,
+      Cor: this.categoria.Cor
+    };
+    if (NovaCategoria.Nome == "") {
+      alert("Insira um nome para a Categoria")
+    } else {
+      console.log(NovaCategoria)
+      this.listaCategoria.push(NovaCategoria)
+      localStorage.setItem('listaCategoria', JSON.stringify(this.listaCategoria));
+      this.categoria.Nome = "";
+    }
+
+
+
+
+  }
+
+
+  excluirCategoria(categoria: Categoria): void {
+    const index = this.listaCategoria.indexOf(categoria);
+
+    if (index !== -1) {
+      this.listaCategoria.splice(index, 1);
+    }
+    localStorage.setItem('listaCategoria', JSON.stringify(this.listaCategoria))
+  }
+
 }
